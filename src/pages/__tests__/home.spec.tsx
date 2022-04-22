a0e98d870e3aff98fffddb9804241138adcfc751
cindy-sf <c.saintfleurant@gmail.com>
2022-04-22 13:34:51 +0200

    feat: implement all the briks on the home page

--- src/pages/__tests__/home.spec.tsx
+++ src/pages/__tests__/home.spec.tsx
@@ -1,20 +1,124 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { organizationStub } from '@src/__mocks__/stubs/organization'
 
 import Home from '../index.page'
 
+jest.mock('next/dynamic', () => ({
+  __esModule: true,
+  default: (...props: any) => {
+    const dynamicModule = jest.requireActual('next/dynamic')
+    const dynamicActualComp = dynamicModule.default
+    const RequiredComponent = dynamicActualComp(props[0])
+    RequiredComponent.preload
+      ? RequiredComponent.preload()
+      : RequiredComponent.render.preload()
+    return RequiredComponent
+  },
+}))
+
 describe('Home', () => {
-  describe('render', () => {
-    it('should display the page title correctly', () => {
+  afterEach(jest.clearAllMocks)
+
+  describe('with organization infos', () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(organizationStub),
+      })
+    ) as jest.Mock
+
+    it('should display the organization infos', async () => {
       // GIVEN
       render(<Home />)
 
       // THEN
+      expect(await screen.findByText(organizationStub.name)).toBeInTheDocument()
       expect(
-        screen.getByText(
-          'Discover and deploy boilerplate example Next.js projects.',
-          { exact: true }
-        )
+        screen.getByText(organizationStub.jobs[0].name)
       ).toBeInTheDocument()
+    })
+
+    describe('without search result', () => {
+      it('should display the no result screen without find result on search', async () => {
+        // GIVEN
+        render(<Home />)
+
+        // WHEN
+        userEvent.click(await screen.findByPlaceholderText('Department'))
+        userEvent.click(screen.getByRole('option', { name: 'Tech' }))
+        userEvent.type(screen.getByPlaceholderText('Search a job'), 'azertyuio')
+
+        // THEN
+        expect(
+          screen.getByText('No job found for your search...', { exact: true })
+        ).toBeInTheDocument()
+      })
+
+      it('should reset the search form by clicking on no result reset button', async () => {
+        // GIVEN
+        render(<Home />)
+
+        // WHEN
+        userEvent.click(await screen.findByPlaceholderText('Department'))
+        userEvent.click(screen.getByRole('option', { name: 'Tech' }))
+        userEvent.type(screen.getByPlaceholderText('Search a job'), 'azertyuio')
+
+        // AND
+        userEvent.click(screen.getByRole('button', { name: 'Reset filters' }))
+
+        // THEN
+        expect(
+          screen.getByText(organizationStub.jobs[0].name)
+        ).toBeInTheDocument()
+      })
+    })
+  })
+
+  describe('without organization infos', () => {
+    describe('loader', () => {
+      it('should display the loader when the data is fetching', () => {
+        // GIVEN
+        render(<Home />)
+
+        // THEN
+        expect(screen.getAllByTitle('Chargement...')).toBeDefined()
+      })
+    })
+
+    describe('error', () => {
+      beforeEach(() => {
+        global.fetch = jest.fn(() =>
+          Promise.resolve({
+            json: () => Promise.reject('error'),
+          })
+        ) as jest.Mock
+      })
+
+      it('should display an error screen on error', async () => {
+        // GIVEN
+        render(<Home />)
+
+        // THEN
+        expect(
+          await screen.findByText('Unfortunately an error has occurred...', {
+            exact: true,
+          })
+        ).toBeDefined()
+      })
+
+      it('should refetch the organization by clicking on retry button', async () => {
+        // GIVEN
+        render(<Home />)
+
+        // WHEN
+        userEvent.click(await screen.findByRole('button', { name: 'Retry' }))
+
+        // THEN
+        await waitFor(() => {
+          expect(global.fetch).toHaveBeenCalledTimes(2)
+        })
+      })
     })
   })
 })
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,22 +1,96 @@
-import Head from 'next/head'
-import type { VFC } from 'react'
-import { Text } from '@welcome-ui/text'
+import dynamic from 'next/dynamic'
+import React, { ReactElement, useEffect, useState, VFC } from 'react'
+import { useForm } from 'react-hook-form'
 
-const Home: VFC = () => {
+import { getOrganizationInfos } from '@src/services'
+import { groupFilters } from '@src/utils/filter'
+
+import Layout from '@components/Layout'
+
+import type { OptionValue } from '@welcome-ui/select'
+import type { Jobs, Organization } from '@src/types'
+
+const Error = dynamic(() => import('@components/Error'))
+const SearchForm = dynamic(() => import('@components/SearchForm'))
+const JobCard = dynamic(() => import('@components/JobCard'))
+const Loader = dynamic(() => import('@components/Loader'))
+const NoResult = dynamic(() => import('@components/NoResult'))
+
+const Home: VFC = (): ReactElement => {
+  const [organization, setOrganization] = useState<Organization>()
+  const [filteredJobs, setFilteredJobs] = useState<Jobs>([])
+  const [shouldDisplayLoader, setShouldDisplayLoader] = useState<boolean>(true)
+  const [shouldDisplayError, setShouldDisplayError] = useState<boolean>(false)
+  const { register, reset, setValue, watch } = useForm({
+    defaultValues: {
+      department: '',
+      jobResearch: '',
+      officeResearch: '',
+    },
+  })
+
+  useEffect(() => {
+    fetchOrganizationInfos()
+  }, [])
+
+  useEffect(() => {
+    const jobs = organization?.jobs
+
+    if (!jobs) return
+
+    const subscription = watch(
+      ({ jobResearch, officeResearch, department }) => {
+        setFilteredJobs(
+          groupFilters(jobs, jobResearch, officeResearch, department)
+        )
+      }
+    )
+
+    return () => subscription.unsubscribe()
+  }, [organization?.jobs, watch])
+
+  const fetchOrganizationInfos = async (): Promise<void> => {
+    try {
+      setShouldDisplayLoader(true)
+
+      const organization = await getOrganizationInfos()
+      setOrganization(organization)
+      setFilteredJobs(organization.jobs)
+    } catch (error) {
+      setShouldDisplayError(true)
+    } finally {
+      setShouldDisplayLoader(false)
+    }
+  }
+
+  const resetForm = (): void => {
+    reset({ jobResearch: '', officeResearch: '', department: '' })
+    setFilteredJobs(organization!.jobs)
+  }
+
   return (
-    <main>
-      <Head>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon.png"
-        />
-        <title>Dream Job</title>
-      </Head>
-      <Text>edededede</Text>
-      <Text>Discover and deploy boilerplate example Next.js projects.</Text>
-    </main>
+    <Layout>
+      <>
+        {shouldDisplayLoader && <Loader />}
+        {shouldDisplayError && <Error onRetry={fetchOrganizationInfos} />}
+        {organization && (
+          <SearchForm
+            organizationName={organization.name}
+            register={register}
+            onDepartmentChange={(department: OptionValue): void => {
+              setValue('department', department as string)
+            }}
+            onReset={resetForm}
+          />
+        )}
+        {!shouldDisplayLoader && !filteredJobs.length && (
+          <NoResult onReset={resetForm} />
+        )}
+        {filteredJobs.map((job) => (
+          <JobCard job={job} key={job.id} />
+        ))}
+      </>
+    </Layout>
   )
 }
 
